d05210ce61fb3066ca188dfb838878ecaefb2006
ced-peps <ced-pep@hotmail.fr>
2021-06-11 10:32:38 +0200

    En cours du 11.06

---
+++ WPF_EMRAC_CONNEXION/Views/ModifierProfil.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_EMRAC_CONNEXION
+{
+    /// <summary>
+    /// Logique d'interaction pour ModifierProfil.xaml
+    /// </summary>
+    public partial class ModifierProfil : Window
+    {
+        public ModifierProfil()
+        {
+            InitializeComponent();
+        }
+    }
+}
